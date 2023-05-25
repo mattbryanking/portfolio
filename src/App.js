66f204a003985b998a5735d33227cef8cea428da
Matthew King <matthewkingstudent@gmail.com>
2023-05-24 22:08:30 -0400

    initial commit, slide working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import WebFont from "webfontloader";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  const handleMouseMove = (event) => {
+    setPosition({ x: event.clientX, y: event.clientY });
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ["Inter", "Roboto Mono"],
+      },
+    });
+  }, []);
+
+  const clipPathStyle = {
+    clipPath: `inset(0px calc(100% - ${position.x}px) 0px 0px)`,
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="title1" style={{ fontFamily: "Inter" }}>
+        MATTHEW KING
+      </h1>
+      <div className="title-container" style={clipPathStyle}>
+        <h1 className="title2" style={{ fontFamily: "Roboto Mono" }}>
+          software-developer
+        </h1>
+      </div>
     </div>
   );
 }
-
-export default App;
