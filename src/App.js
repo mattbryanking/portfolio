3b3a21c47305ff9a3d97c1288d0bf4e24d37674d
Matthew King <matthewkingstudent@gmail.com>
2023-06-01 20:00:42 -0400

    overhauled title screen, added animations

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <>
       <Title/>
+      {/* <div className="other"></div> */}
     </>
   );
 }
--- src/Components/Title/Title.js
+++ src/Components/Title/Title.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Canvas } from "@react-three/fiber";
-import { motion } from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 import WebFont from "webfontloader";
 
 import instawhite from "../../Assets/Icons/insta-white.png";
@@ -23,14 +23,9 @@ const words = [
 export default function Title() {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [currentWordIndex, setCurrentWordIndex] = useState(0);
-
-  const handleMouseMove = (event) => {
-    setPosition({ x: event.clientX, y: event.clientY });
-  };
-
-  const clipPathStyle = {
-    clipPath: `inset(0px calc(100% - ${position.x}px) 0px 0px)`,
-  };
+  const [stopScrollOnLoad, setStopScrollOnLoad] = useState({
+    position: "fixed",
+  });
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -47,36 +42,82 @@ export default function Title() {
       },
     });
 
-    document.addEventListener("mousemove", handleMouseMove);
+    window.scrollTo(0, 0);
 
-    return () => {
-      document.removeEventListener("mousemove", handleMouseMove);
-    };
+    const timer = setTimeout(() => {
+      setStopScrollOnLoad({ position: "absolute" });
+    }, 3000);
+    return () => clearTimeout(timer);
+
+    // document.addEventListener("mousemove", handleMouseMove);
+
+    // return () => {
+    //   document.removeEventListener("mousemove", handleMouseMove);
+    // };
   }, []);
+
+  // const handleMouseMove = (event) => {
+  //   setPosition({ x: event.clientX, y: event.clientY });
+  // };
 
   return (
     <div className="Title">
-      <h1 className="title1" style={{ fontFamily: "Inter" }}>
-        MATTHEW KING
-      </h1>
-      <div className="buttons">
-        <a
-          href="https://github.com/mattbryanking"
-          target="_blank"
-          rel="noreferrer"
+      <div className="title1-container" style={stopScrollOnLoad}>
+        <motion.div
+          initial={{ top: "100%" }}
+          animate={{ top: "0%" }}
+          transition={{
+            ease: [0.215, 0.61, 0.355, 1],
+            duration: 1.5,
+          }}
+          className="blob-container"
         >
-          <img src={githubwhite} alt="github icon" />
-        </a>
-        <a
-          href="https://www.instagram.com/matt.le.fleur/"
-          target="_blank"
-          rel="noreferrer"
+          <Canvas camera={{ position: [0.0, 0.0, 8.0] }}>
+            <Blob />
+          </Canvas>
+        </motion.div>
+        <motion.h1
+          //animate upwards on load, text centered horizontally
+          initial={{ top: "130%" }}
+          animate={{ top: "40%" }}
+          transition={{
+            ease: [0.215, 0.61, 0.355, 1],
+            duration: 1.5,
+            delay: 0.3,
+          }}
+          className="title1"
+          style={{ fontFamily: "Inter" }}
         >
-          <img src={instawhite} alt="instagram icon" />
-        </a>
+          Matthew Bryan King
+        </motion.h1>
+        <div className="buttons">
+          <a
+            href="https://github.com/mattbryanking"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={githubwhite} alt="github icon" />
+          </a>
+          <a
+            href="https://www.instagram.com/matt.le.fleur/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={instawhite} alt="instagram icon" />
+          </a>
+        </div>
       </div>
 
-      <div className="title-container" style={clipPathStyle}>
+      <motion.div
+        initial={{ clipPath: "inset(0 100% 0 0)" }}
+        animate={{ clipPath: "inset(0 0 0 0)" }}
+        transition={{
+          ease: [0.215, 0.61, 0.355, 1],
+          duration: 1.5,
+          delay: 1.5,
+        }}
+        className="title2-container"
+      >
         <Canvas camera={{ position: [0.0, 0.0, 8.0] }}>
           <Blob />
         </Canvas>
@@ -99,7 +140,24 @@ export default function Title() {
             <img src={instablack} alt="instagram icon" />
           </a>
         </div>
-      </div>
+        <motion.div
+          initial={{ top: "-5%" }}
+          animate={{ top: "2.3%" }}
+          transition={{
+            ease: [0.215, 0.61, 0.355, 1],
+            duration: 1.5,
+            delay: 1.7,
+          }}
+          className="links"
+        >
+          <button style={{ fontFamily: "Inter" }} className="button-left">
+            MATTHEW KING
+          </button>
+          <button style={{ fontFamily: "Inter" }}>about</button>
+          <button style={{ fontFamily: "Inter" }}>projects</button>
+          <button style={{ fontFamily: "Inter" }}>contact</button>
+        </motion.div>
+      </motion.div>
     </div>
   );
 }
